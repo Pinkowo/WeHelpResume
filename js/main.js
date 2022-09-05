d0250a450519602eb3104b88578623c7703d1052
Pinkowo <40238691+Pinkowo@users.noreply.github.com>
2022-09-06 06:56:30 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,86 @@
+document.getElementById("url-reciept").addEventListener("click", recieptAni);
+
+function recieptAni() {
+    const menu = document.getElementById("demo-reciept");
+    menu.classList.remove('animate__bounce');
+    setTimeout(() => {
+      menu.classList.add('animate__bounce');
+    }, 500);
+}
+
+
+document.getElementById("url-flickr").addEventListener("click", flickrAni);
+
+function flickrAni() {
+    const menu = document.getElementById("demo-flickr");
+    menu.classList.remove('animate__bounce');
+    setTimeout(() => {
+      menu.classList.add('animate__bounce');
+    }, 500);
+}
+
+
+document.getElementById("url-psycho").addEventListener("click", psychoAni);
+
+function psychoAni() {
+    const menu = document.getElementById("demo-psycho");
+    menu.classList.remove('animate__bounce');
+    setTimeout(() => {
+      menu.classList.add('animate__bounce');
+    }, 500);
+}
+
+
+document.getElementById("url-mask").addEventListener("click", maskAni);
+
+function maskAni() {
+    const menu = document.getElementById("demo-mask");
+    menu.classList.remove('animate__bounce');
+    setTimeout(() => {
+      menu.classList.add('animate__bounce');
+    }, 500);
+}
+
+
+document.getElementById("url-exam").addEventListener("click", examAni);
+
+function examAni() {
+    const menu = document.getElementById("demo-exam");
+    menu.classList.remove('animate__bounce');
+    setTimeout(() => {
+      menu.classList.add('animate__bounce');
+    }, 500);
+}
+
+
+document.getElementById("url-ig").addEventListener("click", igAni);
+
+function igAni() {
+    const menu = document.getElementById("demo-ig");
+    menu.classList.remove('animate__bounce');
+    setTimeout(() => {
+      menu.classList.add('animate__bounce');
+    }, 500);
+}
+
+
+document.getElementById("url-anony").addEventListener("click", anonyAni);
+
+function anonyAni() {
+    const menu = document.getElementById("demo-anony");
+    menu.classList.remove('animate__bounce');
+    setTimeout(() => {
+      menu.classList.add('animate__bounce');
+    }, 500);
+}
+
+
+document.getElementById("url-bak").addEventListener("click", bakAni);
+
+function bakAni() {
+    const menu = document.getElementById("demo-bak");
+    menu.classList.remove('animate__bounce');
+    setTimeout(() => {
+      menu.classList.add('animate__bounce');
+    }, 500);
+}
